fdc687158156ea3e7329afb53f021d492da24da1
Alex <alex.yatseiko@gmail.com>
2020-01-18 13:05:07 +0200

    User schema is added

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,13 +1,34 @@
 const { Router } = require('express')
+const bcrypt = require('bcryptjs')
+const User = require('../models/User')
 const router = Router()
 
 // /api/auth/register
-rotuer.post('/register', async (req, res) => {
+router.post('/register', async (req, res) => {
+    try {
+     
+        const {email, password} = req.body
 
+        const candidate = await User.findOne({ email })
+
+        if (candidate) {
+            return res.status(400).json({ message: 'There is already a user with this email'})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12)
+        const user = new User({ email, password: hashedPassword })
+
+        await user.save()
+
+        res.status(201).json({ message: 'User is created' })
+
+    } catch(e) {
+        res.status(500).json({ message: 'Something went wrong on the server'})
+    }
 })
 
 // /api/auth/login
-rotuer.post('/login', async (req, res) => {
+router.post('/login', async (req, res) => {
 
 })
 
