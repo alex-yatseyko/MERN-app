819b7d2e2ede679ce6bfc75b16216ebd12676cd5
Alex <alex.yatseiko@gmail.com>
2020-01-18 12:43:21 +0200

    db is connected

--- app.js
+++ app.js
@@ -6,11 +6,17 @@ const mongoose = require('mongoose')
 
 const app = express();
 
+app.use('/api/auth', require('./routes/auth.routes'))
+
 const PORT = config.get('port') || 5000
 
 async function start() {
     try {
-        await mongoose.connect(config.get('mongoUri'))
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
     } catch (e) {
         console.log('Server error', e.message)
         process.exit(1)
---
+++ routes/auth.routes.js
@@ -0,0 +1,14 @@
+const { Router } = require('express')
+const router = Router()
+
+// /api/auth/register
+rotuer.post('/register', async (req, res) => {
+
+})
+
+// /api/auth/login
+rotuer.post('/login', async (req, res) => {
+
+})
+
+module.exports = router
