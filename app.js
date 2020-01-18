cf5b39492ddbe953a11e9ac85d552007056077a5
Alex <alex.yatseiko@gmail.com>
2020-01-18 11:39:34 +0200

    gitignore added

--- app.js
+++ app.js
@@ -2,9 +2,21 @@
 
 const express = require('express')
 const config = require('config')
+const mongoose = require('mongoose')
 
 const app = express();
 
 const PORT = config.get('port') || 5000
+
+async function start() {
+    try {
+        await mongoose.connect()
+    } catch (e) {
+        console.log('Server error', e.message)
+        process.exit(1)
+    }
+}
+
+start()
 
 app.listen(PORT, () => { console.log(`App has been started on port ${PORT}`) })
